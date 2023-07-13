68cdaa93312a6afbfb4593f2fc454999ed462115
ankrudov <andre.vasquez.14@gmail.com>
2023-07-12 22:13:12 -0500

    DEV-1 added update view and update password view need delete view

--- user_management/urls.py
+++ user_management/urls.py
@@ -1,7 +1,9 @@
 from django.urls import include, path
-from .views import CreateUserView, LoginView
+from .views import CreateUserView, LoginView, UpdateUserView, UpdatePasswordView
 
 urlpatterns = [
     path('users/create/',CreateUserView.as_view(), name='create-user'),
     path('users/login/', LoginView.as_view(), name='login-user'),
+    path('users/update-user/<int:pk>/',UpdateUserView.as_view(),name='update-user'),
+    path('users/update-password/',UpdatePasswordView.as_view(), name='update-password'),
 ]
--- user_management/views.py
+++ user_management/views.py
@@ -1,19 +1,21 @@
-from django.core.exceptions import ObjectDoesNotExist
-from django.contrib.auth import get_user_model
-from .models import CustomUserV2
-from django.utils import timezone
-from rest_framework import generics, status
-from rest_framework.response import Response
-from .serializers import UserSerializer
-from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
-from rest_framework_simplejwt.views import TokenObtainPairView
 from datetime import timedelta
 
-# Create your views here.
+from .models import CustomUserV2
+from django.shortcuts import get_object_or_404
+from django.utils import timezone
+from django.core.exceptions import ObjectDoesNotExist
+from rest_framework import generics, status
+from rest_framework.response import Response
+from .serializers import UserSerializer, UpdatePasswordSerializer
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework.permissions import IsAuthenticated
+
+# CreateUserView creates a user
 class CreateUserView(generics.CreateAPIView):
     serializer_class = UserSerializer
 
-    def post(self, request, *args, **kwargs):
+    def post(self, request):
         #validate the serializer, checking if valid
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
@@ -81,3 +83,49 @@ class LoginView(TokenObtainPairView):
             'access_token': token.get('access'),
             'refresh_token':token.get('refresh')
         }, status=200)
+    
+# UpdateUserView is allows the user to update their information
+class UpdateUserView(generics.UpdateAPIView): 
+    #make sure user is authenticated before allowing access
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self, pk):
+        try:
+            return CustomUserV2.objects.get(pk=pk)
+        except CustomUserV2.DoesNotExist:
+            return Response({'error':'User does not exist'},status=status.HTTP_404_NOT_FOUND)
+        
+    def put(self, request, pk, format=None):
+        #get user object from the db
+        user = self.get_object(pk)
+        #if the user requesting to update isnt the same as the user from the db, send denied
+        if not request.user == user:
+            return Response({'denied:':'Access Denied'},status=status.HTTP_403_FORBIDDEN)
+        serializer = UserSerializer(user, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+#UpdatePasswordView checks a users old password match, and updates with new password
+class UpdatePasswordView(generics.UpdateAPIView):
+    permission_classes = [IsAuthenticated]
+
+    def put(self, request):
+        #get old password
+        old_password = request.data.get('old_password')
+        # Get the user from the authenticated request.
+        user = get_object_or_404(CustomUserV2, username=request.user.username)
+
+        ##if the old password does not match return error
+        if not user.check_password(old_password):
+            return Response({'error':'Password does not match'}, status=status.HTTP_403_FORBIDDEN)
+        
+        ##if it does match serialize the data
+        serializer = UpdatePasswordSerializer(user, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'success':'Password updated'}, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+#DeleteUserView, verify password, if it matches delete
